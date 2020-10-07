32098657ffe8f9ae5ee6d282f0aab28c378236c8
aleveji <alejandrovegajimenez@gmail.com>
2020-10-07 18:46:04 +0200

    Fichero receta.js añadido closes #3

---
+++ src/receta.js
@@ -0,0 +1,12 @@
+class Receta {
+    constructor(titulo, descr, ingr, prep) {
+        this.titulo = titulo; //titulo
+        this.descr = descr; //descripcion
+        this.ingr = ingr; //ingredientes
+        this.prep = prep; //preparacion
+    }
+    
+    valorarReceta() {
+        //...
+    }
+}
