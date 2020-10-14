0175691e0c5cff465114db70aa5212356fd8db35
aleveji <alejandrovegajimenez@gmail.com>
2020-10-14 17:21:14 +0200

    Fichero usuario.js añadido closes #7

---
+++ src/usuario.js
@@ -0,0 +1,17 @@
+class Usuario {
+    constructor(id, nombre, apodo, mail) {
+        this.id = id; //identificador
+        this.nombre = nombre; //nombre
+        this.apodo = apodo; //apodo
+        this.mail = mail; //correo electronico
+        this.idsRecFav = null; //ids recetas favoritas
+    }
+    
+    anadirRecetaFavorita(idReceta) { //metodo para marcar una receta como favorita
+        //...
+    }
+    
+    anadirComentario(this.id, idReceta, coment){ //metodo para anadir un comentario a una receta
+        //...
+    }
+}
