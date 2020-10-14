6b16b5c388458d4cc6911346403342fdb03e22ba
aleveji <alejandrovegajimenez@gmail.com>
2020-10-14 17:19:57 +0200

    Fichero receta.js actualizado #3

--- src/receta.js
+++ src/receta.js
@@ -1,12 +1,14 @@
 class Receta {
-    constructor(titulo, descr, ingr, prep) {
+    constructor(id, titulo, descr, ingr, prep) {
+        this.id = id; //identificador
         this.titulo = titulo; //titulo
         this.descr = descr; //descripcion
         this.ingr = ingr; //ingredientes
         this.prep = prep; //preparacion
+        this.nota = null; //nota media receta
     }
     
-    valorarReceta() {
+    valorarReceta(nota) { //metodo para valorar una receta
         //...
     }
 }
