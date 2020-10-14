eac297e6932421039a7bcda4a1e7cd6f289c55ee
aleveji <alejandrovegajimenez@gmail.com>
2020-10-14 17:22:08 +0200

    Fichero comentario.js añadido closes #8

---
+++ src/comentario.js
@@ -0,0 +1,11 @@
+class Comentario {
+    constructor(idUsuario, idReceta, coment) {
+        this.idUsuario = idUsuario; //identificador
+        this.idReceta = idReceta; //titulo
+        this.coment = coment; //comentario
+    }
+    
+    valorarReceta(nota) { //metodo para valorar una receta
+        //...
+    }
+}
